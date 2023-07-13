76cf3b2b0f906349d9b14d9db15e78eda834c63e
Elan <elankatheryne@gmail.com>
2023-07-13 04:41:25 +0300

    add api endpoints

---
+++ src/apiUtil.js
@@ -0,0 +1,9 @@
+// beeceptor api
+export const BUSINESS_URL = 'https://businesses.free.beeceptor.com/businesses';
+export const ORDERS_URL = 'https://businesses.free.beeceptor.com/businesses/orders';
+
+// ngrok api
+// export const ngrokUrl = 'https://03b8-105-163-158-69.ngrok-free.app';
+// export const endpoint = '/v1/order/business/1';
+
+// export const API_URL = `${ngrokUrl}${endpoint}`;
